62028a5bc435f12bf45b6c726240c34b6f3397da
Bhavinvora10 <bhavinvora7940@gmail.com>
2024-08-17 17:28:54 +0530

    officer create api in usercontroller

---
+++ Controllers/userController.js
@@ -0,0 +1,21 @@
+const { USER } = require("../Constant/types");
+const User = require("../Model/User");
+const catchAsync = require("../Utils/catchAsync");
+
+exports.createOfficer = catchAsync(async (req, res, next) => {
+  const newOfficer = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    pin: req.body.pin,
+    pinConfirm: req.body.pinConfirm,
+    role: USER.OFFICER,
+    number: req.body.number,
+    email: req.body.email,
+    profileImg: req.body.profileImg,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: { officer: newOfficer },
+  });
+});
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const authController = require('./../Controllers/authController');
+const authController = require("./../Controllers/authController");
+const userController = require("./../Controllers/userController");
+const { ADMIN, SUPER_ADMIN } = require("./../Constant/types").USER;
 
 const router = express.Router();
 
-router.post('/signUp', authController.signUp);
-router.post('/login', authController.login);
+router.post("/signUp", authController.signUp);
+router.post("/login", authController.login);
+
+router.use(authController.protect);
+
+router.post(
+  "/createOfficer",
+  authController.restrictTo(ADMIN, SUPER_ADMIN),
+  userController.createOfficer
+);
 
 module.exports = router;
