3aa7ceb92592eb4fd282c9ea49b3233ca633121c
Nikunj Gajera <89524070+nikunj05122@users.noreply.github.com>
2024-08-19 14:34:34 +0530

    Delete Middleware directory

--- Middleware/response.js
+++
@@ -1,7 +0,0 @@
-module.exports = response = (res, statusCode, success, message, data = null) => {
-    return res.status(statusCode).json({
-        success,
-        message,
-        data,
-    });
-}
