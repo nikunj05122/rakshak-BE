a67c6ea504e8632c88498f45c749f90921fbfbcd
Nikunj Gajera <89524070+nikunj05122@users.noreply.github.com>
2024-08-19 14:34:52 +0530

    Delete Utils directory

--- Utils/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-
-const { firebaseAdminConfig } = require('./../constant/configFireBase');
-
-admin.initializeApp({
-    credential: admin.credential.cert(firebaseAdminConfig),
-});
-
-module.exports = { admin };
